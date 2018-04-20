79f5a58429f05da7b03ce9b9d9e8e9d219dec19f
Colby Garland <colbygarland@gmail.com>
2018-04-20 09:26:32 -0600

    working on home page

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@ jQuery(window).bind("load", function() {
   'use strict';
   jQuery(function($) {
 
-    $('.slider').slick();
+    $('.slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
 
   });
 });
